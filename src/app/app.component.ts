c729ddc149f8fc3001de1fbbcba795acab85c227
darshangit <darshans1989@gmail.com>
2018-01-10 20:14:44 +0530

    Routing pages and roots added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './user/auth.service';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'pm-app',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+    pageTitle: string = 'Acme Product Management';
+
+    constructor(private authService: AuthService) { }
+
+    logOut(): void {
+        this.authService.logout();
+        console.log('Log out');
+    }
 }
